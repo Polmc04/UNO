e2de1ee0bbc381cc0989cf1f5ac1747ec0391d7f
Pol Miró <polmirocarreno@gmail.com>
2024-04-16 15:57:06 +0200

    Versión 2 mejora cliente

--- cliente/WindowsFormsApplication1/Form1.Designer.cs
+++ cliente/WindowsFormsApplication1/Form1.Designer.cs
@@ -55,25 +55,28 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 29);
+            this.label2.Location = new System.Drawing.Point(32, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 24);
+            this.label2.Size = new System.Drawing.Size(78, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             // 
             // nombre
             // 
-            this.nombre.Location = new System.Drawing.Point(169, 34);
+            this.nombre.Location = new System.Drawing.Point(225, 42);
+            this.nombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nombre.Name = "nombre";
-            this.nombre.Size = new System.Drawing.Size(164, 20);
+            this.nombre.Size = new System.Drawing.Size(217, 22);
             this.nombre.TabIndex = 3;
             // 
             // Connect
             // 
             this.Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Connect.Location = new System.Drawing.Point(1208, 12);
+            this.Connect.Location = new System.Drawing.Point(1842, 17);
+            this.Connect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Connect.Name = "Connect";
-            this.Connect.Size = new System.Drawing.Size(147, 53);
+            this.Connect.Size = new System.Drawing.Size(196, 65);
             this.Connect.TabIndex = 4;
             this.Connect.Text = "Connect";
             this.Connect.UseVisualStyleBackColor = true;
@@ -82,9 +85,10 @@
             // Send
             // 
             this.Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Send.Location = new System.Drawing.Point(241, 157);
+            this.Send.Location = new System.Drawing.Point(321, 193);
+            this.Send.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Send.Name = "Send";
-            this.Send.Size = new System.Drawing.Size(94, 39);
+            this.Send.Size = new System.Drawing.Size(125, 48);
             this.Send.TabIndex = 5;
             this.Send.Text = "Send";
             this.Send.UseVisualStyleBackColor = true;
@@ -101,9 +105,11 @@
             this.groupBox1.Controls.Add(this.Send);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.nombre);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(341, 204);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(455, 251);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Peticion";
@@ -112,9 +118,10 @@
             // 
             this.Remove.AutoSize = true;
             this.Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Remove.Location = new System.Drawing.Point(28, 165);
+            this.Remove.Location = new System.Drawing.Point(37, 203);
+            this.Remove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Remove.Name = "Remove";
-            this.Remove.Size = new System.Drawing.Size(189, 24);
+            this.Remove.Size = new System.Drawing.Size(234, 29);
             this.Remove.TabIndex = 11;
             this.Remove.TabStop = true;
             this.Remove.Text = "Remove my account =(";
@@ -122,9 +129,10 @@
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(170, 60);
+            this.password.Location = new System.Drawing.Point(227, 74);
+            this.password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(164, 20);
+            this.password.Size = new System.Drawing.Size(217, 22);
             this.password.TabIndex = 10;
             this.password.UseSystemPasswordChar = true;
             // 
@@ -132,9 +140,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(24, 60);
+            this.label1.Location = new System.Drawing.Point(32, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 24);
+            this.label1.Size = new System.Drawing.Size(120, 29);
             this.label1.TabIndex = 9;
             this.label1.Text = "Password";
             // 
@@ -142,9 +151,10 @@
             // 
             this.LogIn.AutoSize = true;
             this.LogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LogIn.Location = new System.Drawing.Point(28, 135);
+            this.LogIn.Location = new System.Drawing.Point(37, 166);
+            this.LogIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LogIn.Name = "LogIn";
-            this.LogIn.Size = new System.Drawing.Size(72, 24);
+            this.LogIn.Size = new System.Drawing.Size(87, 29);
             this.LogIn.TabIndex = 7;
             this.LogIn.TabStop = true;
             this.LogIn.Text = "Log In";
@@ -154,9 +164,10 @@
             // 
             this.SingUp.AutoSize = true;
             this.SingUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SingUp.Location = new System.Drawing.Point(28, 105);
+            this.SingUp.Location = new System.Drawing.Point(37, 129);
+            this.SingUp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SingUp.Name = "SingUp";
-            this.SingUp.Size = new System.Drawing.Size(88, 24);
+            this.SingUp.Size = new System.Drawing.Size(109, 29);
             this.SingUp.TabIndex = 8;
             this.SingUp.TabStop = true;
             this.SingUp.Text = "Sing Up!";
@@ -165,9 +176,10 @@
             // Disconnect
             // 
             this.Disconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Disconnect.Location = new System.Drawing.Point(1208, 98);
+            this.Disconnect.Location = new System.Drawing.Point(1842, 93);
+            this.Disconnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Disconnect.Name = "Disconnect";
-            this.Disconnect.Size = new System.Drawing.Size(147, 53);
+            this.Disconnect.Size = new System.Drawing.Size(196, 65);
             this.Disconnect.TabIndex = 10;
             this.Disconnect.Text = "Disconnect";
             this.Disconnect.UseVisualStyleBackColor = true;
@@ -176,9 +188,10 @@
             // MasPartidas
             // 
             this.MasPartidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MasPartidas.Location = new System.Drawing.Point(12, 231);
+            this.MasPartidas.Location = new System.Drawing.Point(16, 284);
+            this.MasPartidas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MasPartidas.Name = "MasPartidas";
-            this.MasPartidas.Size = new System.Drawing.Size(147, 79);
+            this.MasPartidas.Size = new System.Drawing.Size(196, 97);
             this.MasPartidas.TabIndex = 11;
             this.MasPartidas.Text = "Jugador con mas partidas";
             this.MasPartidas.UseVisualStyleBackColor = true;
@@ -187,9 +200,10 @@
             // MasELO
             // 
             this.MasELO.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MasELO.Location = new System.Drawing.Point(12, 316);
+            this.MasELO.Location = new System.Drawing.Point(16, 389);
+            this.MasELO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MasELO.Name = "MasELO";
-            this.MasELO.Size = new System.Drawing.Size(147, 79);
+            this.MasELO.Size = new System.Drawing.Size(196, 97);
             this.MasELO.TabIndex = 12;
             this.MasELO.Text = "Jugador con mas ELO";
             this.MasELO.UseVisualStyleBackColor = true;
@@ -198,9 +212,10 @@
             // ColorCarta
             // 
             this.ColorCarta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ColorCarta.Location = new System.Drawing.Point(12, 401);
+            this.ColorCarta.Location = new System.Drawing.Point(16, 494);
+            this.ColorCarta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ColorCarta.Name = "ColorCarta";
-            this.ColorCarta.Size = new System.Drawing.Size(147, 79);
+            this.ColorCarta.Size = new System.Drawing.Size(196, 97);
             this.ColorCarta.TabIndex = 13;
             this.ColorCarta.Text = "Color de la carta +4";
             this.ColorCarta.UseVisualStyleBackColor = true;
@@ -209,9 +224,10 @@
             // DimeConectados
             // 
             this.DimeConectados.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DimeConectados.Location = new System.Drawing.Point(12, 486);
+            this.DimeConectados.Location = new System.Drawing.Point(16, 598);
+            this.DimeConectados.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DimeConectados.Name = "DimeConectados";
-            this.DimeConectados.Size = new System.Drawing.Size(147, 79);
+            this.DimeConectados.Size = new System.Drawing.Size(196, 97);
             this.DimeConectados.TabIndex = 14;
             this.DimeConectados.Text = "Dime Conectados";
             this.DimeConectados.UseVisualStyleBackColor = true;
@@ -220,9 +236,10 @@
             // EmpezarPartida
             // 
             this.EmpezarPartida.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EmpezarPartida.Location = new System.Drawing.Point(1154, 779);
+            this.EmpezarPartida.Location = new System.Drawing.Point(1766, 894);
+            this.EmpezarPartida.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.EmpezarPartida.Name = "EmpezarPartida";
-            this.EmpezarPartida.Size = new System.Drawing.Size(201, 53);
+            this.EmpezarPartida.Size = new System.Drawing.Size(268, 65);
             this.EmpezarPartida.TabIndex = 15;
             this.EmpezarPartida.Text = "Empezar partida";
             this.EmpezarPartida.UseVisualStyleBackColor = true;
@@ -231,9 +248,10 @@
             // Player2
             // 
             this.Player2.AutoSize = true;
-            this.Player2.Location = new System.Drawing.Point(456, 316);
+            this.Player2.Location = new System.Drawing.Point(608, 389);
+            this.Player2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Player2.Name = "Player2";
-            this.Player2.Size = new System.Drawing.Size(45, 13);
+            this.Player2.Size = new System.Drawing.Size(56, 16);
             this.Player2.TabIndex = 16;
             this.Player2.Text = "Player 2";
             this.Player2.Visible = false;
@@ -242,9 +260,10 @@
             // Player4
             // 
             this.Player4.AutoSize = true;
-            this.Player4.Location = new System.Drawing.Point(951, 316);
+            this.Player4.Location = new System.Drawing.Point(1268, 389);
+            this.Player4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Player4.Name = "Player4";
-            this.Player4.Size = new System.Drawing.Size(45, 13);
+            this.Player4.Size = new System.Drawing.Size(56, 16);
             this.Player4.TabIndex = 17;
             this.Player4.Text = "Player 4";
             this.Player4.Visible = false;
@@ -252,9 +271,10 @@
             // labelYourCards
             // 
             this.labelYourCards.AutoSize = true;
-            this.labelYourCards.Location = new System.Drawing.Point(715, 522);
+            this.labelYourCards.Location = new System.Drawing.Point(953, 642);
+            this.labelYourCards.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelYourCards.Name = "labelYourCards";
-            this.labelYourCards.Size = new System.Drawing.Size(59, 13);
+            this.labelYourCards.Size = new System.Drawing.Size(74, 16);
             this.labelYourCards.TabIndex = 18;
             this.labelYourCards.Text = "Your Cards";
             this.labelYourCards.Visible = false;
@@ -262,18 +282,19 @@
             // Player3
             // 
             this.Player3.AutoSize = true;
-            this.Player3.Location = new System.Drawing.Point(715, 203);
+            this.Player3.Location = new System.Drawing.Point(953, 250);
+            this.Player3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Player3.Name = "Player3";
-            this.Player3.Size = new System.Drawing.Size(45, 13);
+            this.Player3.Size = new System.Drawing.Size(56, 16);
             this.Player3.TabIndex = 19;
             this.Player3.Text = "Player 3";
             this.Player3.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1365, 844);
+            this.ClientSize = new System.Drawing.Size(1924, 972);
             this.Controls.Add(this.Player3);
             this.Controls.Add(this.labelYourCards);
             this.Controls.Add(this.Player4);
@@ -286,8 +307,11 @@
             this.Controls.Add(this.Disconnect);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Connect);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
